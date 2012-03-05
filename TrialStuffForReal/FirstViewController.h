e14bb0ec491e768f7e5559597bbe565f6e6a1636
NathanKetzner <nathan.ketzner@nwktc.edu>
2012-03-05 08:25:03 -0700

    Initial Commit

---
+++ TrialStuffForReal/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TrialStuffForReal
+//
+//  Created by Nathan Ketzner on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ TrialStuffForReal/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TrialStuffForReal
+//
+//  Created by Nathan Ketzner on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
